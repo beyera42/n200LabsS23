fda1e6762b14d30f3d3e729bf409247d2e004827
beyera42 <beyer2272@gmail.com>
2023-01-26 13:16:57 -0500

    Animations Algorithm
    This is the plan so far. The 3 items that I have chosen are Counter, Puck Side, and Ever Larger, until not. I have also placed the directions that I need to follow for each part of the assignment. I intend on featuring all of these choices together on the same file, but I can't promise anything.

---
+++ labs/lab2/js/app.js
@@ -0,0 +1,17 @@
+//Date:1/26/2023, Name: Alex Beyer, Class #: N220
+
+//Assignment: Animation and Conditional Operators Exercise
+
+//1st Choice: Counter
+
+//Create an application that outputs a number to the console every draw call of P5. This number should start at zero, and increase by one every frame.
+
+//2nd Choice: Puck Side
+
+//Canvas size: 400x300
+
+//Create an application that draws a circle where the mouse is at. (Use  the P5 variables mouseX and mouseY for this). When the mouse is on the right half of the canvas, draw the circle in red. When the mouse is on the left side of the canvas, draw the circle in blue.
+
+//3rd Choice: Ever Larger, until not
+
+//Create an application that draws a circle to the center of the screen every update frame, starting with a diameter of 1. Using a global variable, change increase the diameter of the circle by 1 every frame. Once the circle gets a diameter of 200, set its diameter back to 1.
