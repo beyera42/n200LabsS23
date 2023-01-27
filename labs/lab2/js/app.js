f743d0490ce03dbeadbde918bcf62a990343989a
beyera42 <beyer2272@gmail.com>
2023-01-26 23:23:25 -0500

    Update app.js
    This will hopefully get the code to show up on the browser.

--- labs/lab2/js/app.js
+++ labs/lab2/js/app.js
@@ -34,3 +34,5 @@ function circleo(x, y) {
 //3rd Choice: Ever Larger, until not
 
 //Create an application that draws a circle to the center of the screen every update frame, starting with a diameter of 1. Using a global variable, change increase the diameter of the circle by 1 every frame. Once the circle gets a diameter of 200, set its diameter back to 1.
+
+//Operation
